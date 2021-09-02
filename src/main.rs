aec04aa2cd5c76a7bbcb21f18d239f8fd067c7b6
4got <4gotthehero@gmail.com>
2021-09-02 03:35:03 +0300

    Implement as struct

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,71 @@ use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{self, prelude::*, BufReader};
 
-struct TodoList {
-    todos: [String],
+const TODOLIST_PATH: &str = "list.txt";
+
+struct TodoList<T> {
+    todos: Vec<T>,
 }
 
-impl TodoList {
-    // fn from_file(file: &File) -> TodoList {
-    //     TodoList {
-    //         todos: BufReader::new(file).lines().collect(),
-    //     }
-    // }
-    fn print_todo_list(file: &File) -> std::io::Result<()> {
-        println!("Hi your todo list:");
+impl TodoList<Box<String>> {
+    fn new(file: &File) -> TodoList<Box<String>> {
+        let mut todos = Vec::new();
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let todo = line.unwrap();
+            if todo.len() > 0 {
+                todos.push(Box::new(todo));
+            }
+        }
+        TodoList { todos }
+    }
+
+    fn print(self) -> Self {
+        for (i, todo) in self.todos.iter().enumerate() {
+            println!("{}. {}", i + 1, todo);
+        }
+        self
+    }
+
+    fn serve(mut file: File) -> std::io::Result<()> {
+        println!("\r\nWant to add something?");
+        let mut todo = String::new();
+        io::stdin().read_line(&mut todo)?;
+        if todo.trim().len() <= 0 {
+            println!("\r\nNothing to add, closing\r\n");
+            return Ok(());
+        }
+
+        println!("\r\nAdd '{}' was successfull, closing\r\n", todo.trim());
+        file.write_all(todo.as_bytes())?;
+        Ok(())
+    }
+
+    fn get_file() -> File {
+        OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(&String::from(TODOLIST_PATH))
+            .unwrap()
+    }
+
+    #[allow(dead_code)]
+    fn print_from_file(file: &File) {
+        println!("Hi, your todo list:");
         let reader = BufReader::new(file);
         for (i, line) in reader.lines().enumerate() {
-            println!("{}. {}", i + 1, line?);
+            println!("{}. {}", i + 1, line.unwrap());
         }
-        Ok(())
     }
 }
 
 fn main() -> std::io::Result<()> {
-    let file_name = String::from("list.txt");
-    let mut file = OpenOptions::new()
-        .read(true)
-        .append(true)
-        .open(&file_name)?;
+    let file = TodoList::get_file();
 
-    // let todo_list = String::new();
+    let todo_list = TodoList::new(&file);
+    todo_list.print();
 
-    // println!("Hi your todo list:");
-    // let reader = BufReader::new(&file);
-    // for (i, line) in reader.lines().enumerate() {
-    //     println!("{}. {}", i + 1, line?);
-    // }
-    // TodoList::from_file(&file);
-    TodoList::print_todo_list(&file)?;
+    TodoList::serve(file)?;
 
-    // file.read_to_string(&mut todo_list)?;
-    // if let Ok(lines) = read_lines
-
-    println!("Want to add something?");
-    let mut todo = String::new();
-    io::stdin().read_line(&mut todo)?;
-    println!("\r\nAdd '{}' was successfull\r\n", todo.trim());
-
-    file.write_all(todo.as_bytes())?;
-
-    // let file = OpenOptions::new().append(true).open()
     Ok(())
 }
