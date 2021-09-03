57c09ed56d14d39a85e109df94e28c27ef981d33
4got <4gotthehero@gmail.com>
2021-09-03 18:50:16 +0300

    Add add/remove methods

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,15 @@ impl TodoList<Box<String>> {
         TodoList { todos }
     }
 
+    fn remove(self, number: u32) -> Self {
+        println!("remove_number = {:?}", number);
+        self
+    }
+
+    fn add() -> std::io::Result<()> {
+        Ok(())
+    }
+
     fn print(self) -> Self {
         for (i, todo) in self.todos.iter().enumerate() {
             println!("{}. {}", i + 1, todo);
@@ -29,34 +38,66 @@ impl TodoList<Box<String>> {
     }
 
     fn serve(mut file: File) -> std::io::Result<()> {
-        println!("\r\nWhat do you want?");
+        let todo_list = TodoList::new(&file);
+        todo_list.print();
 
         //  interface
+        println!("\r\nWhat do you want?\r\n");
+        println!("clear: remove all todos");
+        println!("remove: remove todo");
+        println!("add: add todo");
 
-        println!("\r\nWant to add something?");
-        let mut todo = String::new();
-        io::stdin().read_line(&mut todo)?;
-        if todo.trim().len() <= 0 {
-            println!("\r\nNothing to add, closing\r\n");
-            return Ok(());
+        let mut command = String::new();
+        io::stdin().read_line(&mut command)?;
+        command = command.trim().to_string();
+
+        // clear
+        if command.eq("clear") {
+            file.set_len(0)?;
+            println!("\r\nClear was successfull, closing\r\n");
+            
+        // remove
+        } else if command.eq("remove") {
+            println!("Press number of todo: ");
+            let mut remove_number_string = String::new();
+            io::stdin().read_line(&mut remove_number_string)?;
+
+            let remove_number = remove_number_string.parse::<u32>().unwrap();
+
+            if remove_number > 0 {
+                let todo_list = TodoList::new(&file);
+                todo_list.remove(remove_number);
+            }
+        
+        // add
+        } else if command.eq("add") {
+            println!("\r\nWant to add something?");
+            let mut todo = String::new();
+            io::stdin().read_line(&mut todo)?;
+            if todo.len() <= 0 {
+                println!("\r\nNothing to add, closing\r\n");
+                return Ok(());
+            }
+
+            println!("\r\nAdd '{}' was successfull, closing\r\n", todo.trim());
+            file.write_all(todo.as_bytes())?;
         }
-
-        println!("\r\nAdd '{}' was successfull, closing\r\n", todo.trim());
-        file.write_all(todo.as_bytes())?;
+        
         Ok(())
     }
 
     fn get_file() -> File {
         OpenOptions::new()
             .read(true)
-            .append(true)
+            .write(true)
+            // .append(true)
+            .create(true)
             .open(&String::from(TODOLIST_PATH))
             .unwrap()
     }
 
     #[allow(dead_code)]
     fn print_from_file(file: &File) {
-        println!("Hi, your todo list:");
         let reader = BufReader::new(file);
         for (i, line) in reader.lines().enumerate() {
             println!("{}. {}", i + 1, line.unwrap());
@@ -66,10 +107,6 @@ impl TodoList<Box<String>> {
 
 fn main() -> std::io::Result<()> {
     let file = TodoList::get_file();
-
-    let todo_list = TodoList::new(&file);
-    todo_list.print();
-
     TodoList::serve(file)?;
 
     Ok(())
